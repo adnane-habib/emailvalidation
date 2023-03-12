d12e64bdc51c6ee0997cf9083bab4bbc02fc671a
Adnane Habib <adnanou.habibou@gmail.com>
2023-03-12 21:06:23 +0300

    adding email input from user

--- Email Validation/src/emailvalidation/EmailValid.java
+++ Email Validation/src/emailvalidation/EmailValid.java
@@ -1,10 +1,15 @@
 package emailvalidation;
+import java.util.Scanner;
+
 
 public class EmailValid {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter a valid email ID: ");
+		String emailID = scanner.nextLine();
+		System.out.println("You have entered: "+emailID);
 
 	}
 
